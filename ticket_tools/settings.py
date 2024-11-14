7e7810808cbad04decf2b75f89d26bb1d931e089
Shubham Gawande <shubhampg99@gmail.com>
2024-11-15 00:53:30 +0530

    third commit

--- ticket_tools/settings.py
+++ ticket_tools/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
-import django_heroku
-import dj_database_url
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,10 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT= 'staticfiles'
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 STATICFILES_STORAGE: "whitenoise.storage.CompressedManifestStaticFilesStorage"
-django_heroku.settings(locals())
-# Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
