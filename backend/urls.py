b268ee125f091779ac9d200013e4eb17861e4f3b
Shubham Gawande <shubhampg99@gmail.com>
2024-11-15 01:51:30 +0530

    9th commit

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns=[
     path("create_ticket/",views.create_ticket, name="create_ticket"),
     path("list_ticket/",views.list_ticket, name="list_ticket"),
     path('tickets/<int:ticket_id>/', views.ticket_detail, name='ticket_detail'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('tickets/add_comment/<int:ticket_id>/', views.add_comment, name='add_comment'),
     path('tickets/update_ticket_status/<int:ticket_id>/', views.update_ticket_status, name='update_ticket_status')
 ]
